5d2a0c693bc5ddb495ee1f64eee1c60fa346ea29
Justin Mitchel <jmitchel3@users.noreply.github.com>
2025-06-17 11:24:38 -0600

    15 - Integrate Reflex with Postgres through Dotenv

--- rxconfig.py
+++ rxconfig.py
@@ -1,6 +1,13 @@
+import os
 import reflex as rx
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DATABASE_URL = os.environ.get("DATABASE_URL")
 
 config = rx.Config(
     app_name="podcast_discovery",
     plugins=[rx.plugins.TailwindV3Plugin()],
+    db_url=DATABASE_URL
 )
