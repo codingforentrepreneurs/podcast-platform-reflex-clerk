3c5b6c2e1bae8d091fe4aced43a1c5b01ad6e72b
Justin Mitchel <jmitchel3@users.noreply.github.com>
2025-06-18 15:07:47 -0600

    27 - User and Non User Layout

--- podcast_discovery/layout.py
+++ podcast_discovery/layout.py
@@ -1,16 +1,45 @@
 import reflex as rx
+import reflex_clerk_api as reclerk
 from podcast_discovery.ui.nav import navbar
 
 from podcast_discovery import providers
 
-def root_layout(child:rx.Component, *args, **kwargs):
 
-    return providers.my_clerk_provider(
-        rx.container(
+def non_user_layout(child:rx.Component)-> rx.Component:
+
+    return rx.container(
             navbar(),
             rx.fragment(child),
             rx.logo(),
             width="100%",
             id='my-root-layout'
         )
+
+
+
+def user_layout(child:rx.Component)-> rx.Component:
+
+    return rx.container(
+            rx.fragment(child),
+            rx.logo(),
+            width="100%",
+            id='my-root-layout'
+        )
+
+
+def root_layout(child:rx.Component, *args, **kwargs)-> rx.Component:
+
+    return providers.my_clerk_provider(
+        reclerk.clerk_loading(
+            rx.spinner(),
+        ),
+        reclerk.clerk_loaded(
+            reclerk.signed_in(
+                non_user_layout(child)
+            ),
+            reclerk.signed_out(
+                user_layout(child)
+            ),
+        ),
+        
     )
