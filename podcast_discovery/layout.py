19db677a2676ae4e0270c4dbd174e6efa3d61358
Justin Mitchel <jmitchel3@users.noreply.github.com>
2025-06-18 12:23:19 -0600

    22 - Integrating Clerk with Reflex

--- podcast_discovery/layout.py
+++ podcast_discovery/layout.py
@@ -1,12 +1,16 @@
 import reflex as rx
 from podcast_discovery.ui.nav import navbar
 
+from podcast_discovery import providers
+
 def root_layout(child:rx.Component, *args, **kwargs):
 
-    return rx.container(
-        navbar(),
-        rx.fragment(child),
-        rx.logo(),
-        width="100%",
-        id='my-root-layout'
+    return providers.my_clerk_provider(
+        rx.container(
+            navbar(),
+            rx.fragment(child),
+            rx.logo(),
+            width="100%",
+            id='my-root-layout'
+        )
     )
