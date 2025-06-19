b59328a5991f16bacdb236967fce236b9e694669
Justin Mitchel <jmitchel3@users.noreply.github.com>
2025-06-19 10:22:27 -0600

    33 - Search Results Row Display

--- podcast_discovery/podcasts/state.py
+++ podcast_discovery/podcasts/state.py
@@ -2,8 +2,14 @@ from typing import List, Any
 import reflex as rx
 from podcast_discovery import helpers
 
+
+# key_mapping = {
+#     "trackName": "Title"
+# }
+
 class PodcastSearchState(rx.State):
     results: List[dict] | None = None
+    columns: List[str] = []
     query: str = ""
     loading: bool = False
 
@@ -13,9 +19,10 @@ class PodcastSearchState(rx.State):
         self.query = query
         self.results = None
         self.loading = True
-        api_results = helpers.podcast_search(query, limit=5, attribute="descriptionTerm")
+        api_results = helpers.podcast_search(query, limit=100, attribute="descriptionTerm", sort_by_date=True)
         if len(api_results) > 0:
             print(api_results[0].keys())
+            # self.columns = [key_mapping.get(y) for y in api_results[0].keys()]
         self.loading = False
-        self.results = api_results
+        self.results = api_results[:20]
         # call the search api
--- podcast_discovery/podcasts/table.py
+++ podcast_discovery/podcasts/table.py
@@ -3,11 +3,23 @@ import reflex as rx
 from .state import PodcastSearchState
 
 def search_table_row(data:dict) -> rx.Component:
-
+    """
+    ['episodeUrl', 'collectionViewUrl', 'trackViewUrl', 
+    'artworkUrl60', 'trackTimeMillis', 'contentAdvisoryRating', 
+    'previewUrl', 'artistIds', 'genres', 
+    'episodeGuid', 'releaseDate', 'trackId', 
+    'trackName', 'shortDescription', 'feedUrl', 
+    'closedCaptioning', 'collectionId', 'collectionName', 
+    'kind', 'wrapperType', 'description', 'country', 
+    'artworkUrl600', 'episodeFileExtension', 
+    'episodeContentType', 'artworkUrl160']
+    
+    """
     return rx.table.row(
-                rx.table.row_header_cell(data.get("title")),
-                rx.table.cell("danilo@example.com"),
-                rx.table.cell("Developer"),
+                rx.table.row_header_cell(data.get("trackName")),
+                rx.table.cell(data.get("episodeUrl")),
+                rx.table.cell(data.get("releaseDate")),
+                rx.table.cell(data.get("collectionName")),
             )
 
 
@@ -33,9 +45,10 @@ def search_results_table() -> rx.Component:
     return rx.table.root(
         rx.table.header(
             rx.table.row(
-                rx.table.column_header_cell("Full name"),
-                rx.table.column_header_cell("Email"),
-                rx.table.column_header_cell("Group"),
+                rx.table.column_header_cell("Track Name"),
+                rx.table.column_header_cell("Audio"),
+                rx.table.column_header_cell("Release date"),
+                rx.table.column_header_cell("Podcast Name"),
             ),
         ),
         rx.table.body(
